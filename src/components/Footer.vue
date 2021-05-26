4c0096ddf450de0ee5cc90bf9fd82201433ee737
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-05-26 21:26:35 +0200

    onglet_gauche

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-footer dark padless app inset>
+        <v-card class="flex" tile color="red" :elevation="10">
+            <v-card-text class="py-2" white--text text-center>
+                {{new Date().getFullYear()}} + <strong>AAE IdeaPro</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
       <v-app-bar color="red" dark app>
-          <v-app-bar-nav-icon>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer">
 
           </v-app-bar-nav-icon>
           <v-toolbar-title class="text-uppercase">
@@ -9,16 +9,44 @@
               <span>ideaPro</span>
           </v-toolbar-title>
           <v-spacer></v-spacer>
-          <menu offset-y>
+          <v-menu offset-y>
               <template v-slot:activator="{on}">
-                  
+                  <v-btn text v-on="on">
+                      <v-icon left>expand_more</v-icon>
+                      <span>Menu</span>
+                  </v-btn>
               </template>
-          </menu>
+              <v-list flat>
+                  <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
+                      <v-list-item-title>{{link.text}}</v-list-item-title>
+                  </v-list-item>
+              </v-list>
+          </v-menu>
           <v-btn text>
               <span>Quitter</span>
               <v-icon right>exit_to_app</v-icon>
           </v-btn>
       </v-app-bar>
+      <v-navigation-drawer v-model="drawer" dark app class="red darken-4">
+        <v-layout column align-center>
+            <v-flex class="mt-5">
+                <v-avatar size="100">
+                    <v-img src="../assets/img/img1.png"></v-img>
+                </v-avatar>
+                <p class="white--text subheading mt-1 text-center">Username</p>
+            </v-flex>
+        </v-layout>
+        <v-list flat>
+            <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
+                <v-list-item-acion>
+                    <v-icon>{{link.icon}}</v-icon>
+                </v-list-item-acion>
+                <v-list-item-content>
+                    <v-list-item-title>{{link.text}}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
   </nav>
 </template>
 
@@ -26,16 +54,23 @@
 export default {
     name: 'Navbar',
     data() {
-        drawer: true;
+        return {
+            drawer: true,
         links : [
-            {icon : 'Dashboard', text: 'Dashboard', route: '/'},
+            {icon : 'dashboard', text: 'Dashboard', route: '/'},
             {icon : 'folder', text: 'My Projects', route: '/projects'},
             {icon : 'person', text: 'Team', route: '/team'},
         ]
+        }
+        
     }
 }
 </script>
 
-<style>
+<style scoped>
+
+.border {
+    border-left: 4px solid #0ba518;
+}
 
 </style>
