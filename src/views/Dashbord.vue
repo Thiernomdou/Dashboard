0d9e0f8c232377d7694148ec541892d81dc34901
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2021-06-05 13:04:45 +0200

    card

--- src/views/Dashbord.vue
+++ src/views/Dashbord.vue
@@ -6,17 +6,17 @@
         <v-flex sm6 xs12 md6 lg3>
           <v-card class="ma-3">
             <v-list-item>
-              <v-list-item-avatar tile class="mt-m7">
-                <v-sheet color="green" width="80" height="80" elevation="10">
-                  <v-icon dark large>store</v-icon>
-                </v-sheet>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <div class="overline text-right">Article</div>
-                <v-list-item-title class="headline mb-1 text-right">523615</v-list-item-title>
-                <div><v-divider></v-divider></div>
-              </v-list-item-content>
-            </v-list-item>
+              <v-list-item-avatar tile class="mt-n7">
+              <v-sheet color="green" width="80" height="80" elevation="10">
+                    <v-icon dark large>store</v-icon>
+              </v-sheet>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <div class="overline text-right">Article</div>
+              <v-list-item-title class="headline mb-1 text-right" >523614</v-list-item-title>
+              <div><v-divider></v-divider></div>
+            </v-list-item-content> 
+          </v-list-item>
             <v-card-actions>
               <v-icon text class="ma-2">person</v-icon>
               <div class="overline">Iyad</div>
